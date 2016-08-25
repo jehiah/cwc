6adec4341950122e70969b1107343723afe6f304
Jehiah Czebotar <jehiah@gmail.com>
2016-08-24 22:33:42 -0400

    first pass at giving cwc options

--- src/cmd/cwc/cwc.go
+++ src/cmd/cwc/cwc.go
@@ -2,33 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"db"
+
+	input "github.com/tcnksm/go-input"
 )
 
-func main() {
-	for _, arg := range os.Args[1:] {
-		fmt.Printf("Searching for: %q\n", arg)
-		files, err := db.Default.Find(arg)
-		if err != nil {
-			fmt.Printf("Error: %s\n", err)
-			continue
+func run(action string, args ...string) {
+	switch action {
+	case "search":
+		if len(args) >= 1 {
+			search(args[0])
+		} else {
+			search("")
 		}
-		for i, c := range files {
-			if i == 0 {
-				fmt.Printf("opening: %s %s\n", c, db.Default.FullPath(c))
-				err = db.Default.ShowInFinder(c)
-				if err != nil {
-					fmt.Printf("%s\n", err)
-				}
-				err = db.Default.Edit(c)
-				if err != nil {
-					fmt.Printf("%s\n", err)
-				}
-			} else {
-				fmt.Printf("also found: %s %s\n", c, db.Default.FullPath(c))
+	case "help":
+		fmt.Printf(`cwc -> Cyclists With Cameras
+
+For more information see https://github.com/jehiah/cwc
+`)
+	default:
+		log.Fatalf("not implemented")
+	}
+}
+
+var ui *input.UI
+
+func main() {
+	ui = &input.UI{}
+
+	if len(os.Args) > 1 {
+		run(os.Args[1], os.Args[2:]...)
+	} else {
+		action, _ := ui.Select("", []string{"help", "search", "new", "report"}, &input.Options{
+			Default: "new",
+		})
+		run(action)
+	}
+}
+
+func search(query string) {
+	var err error
+	if query == "" {
+		query, err = ui.Ask("Search for?", &input.Options{Required: true, Loop: true, HideOrder: true})
+		if err != nil {
+			os.Exit(1)
+		}
+		if query == "" {
+			log.Fatalf("missing query")
+		}
+	} else {
+		fmt.Printf("Searching for: %q\n", query)
+	}
+
+	files, err := db.Default.Find(query)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	for i, c := range files {
+		if i == 0 {
+			fmt.Printf("opening: %s %s\n", c, db.Default.FullPath(c))
+			err = db.Default.ShowInFinder(c)
+			if err != nil {
+				fmt.Printf("%s\n", err)
 			}
+			err = db.Default.Edit(c)
+			if err != nil {
+				fmt.Printf("%s\n", err)
+			}
+		} else {
+			fmt.Printf("also found: %s %s\n", c, db.Default.FullPath(c))
 		}
 	}
 }
--- vendor.sh
+++ vendor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gb vendor fetch --revision 709fab3d192d7c62f86043caff1e7e3fb0f42bd8 github.com/rwcarlsen/goexif
-
+gb vendor fetch -no-recurse --revision 709fab3d192d7c62f86043caff1e7e3fb0f42bd8 github.com/rwcarlsen/goexif
+gb vendor fetch -no-recurse --revision a01c4418c4930027868308d73c1fb21f4692add9 github.com/tcnksm/go-input
+gb vendor fetch -no-recurse --revision 351dc6a5bf92a5f2ae22fadeee08eb6a45aa2d93 golang.org/x/crypto/ssh
