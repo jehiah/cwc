70f4a46eaacfa21e400562b164a21c99f2f607b9
Jehiah Czebotar <jehiah@gmail.com>
2023-06-10 22:47:07 -0400

    scripting submission in chromedriver

--- cmd/cwc/submit_311.go
+++ cmd/cwc/submit_311.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/chromedp/chromedp"
@@ -67,6 +69,10 @@ func runSubmitComplaint(d db.ReadWrite, args ...string) error {
 }
 
 func Submit(fc *complaint.FullComplaint) error {
+	hd, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
 	opts := []chromedp.ExecAllocatorOption{
 		chromedp.NoFirstRun,
 		chromedp.NoDefaultBrowserCheck,
@@ -81,7 +87,9 @@ func Submit(fc *complaint.FullComplaint) error {
 		chromedp.Flag("disable-client-side-phishing-detection", true),
 		chromedp.Flag("disable-default-apps", true),
 		chromedp.Flag("disable-dev-shm-usage", true),
-		chromedp.Flag("disable-extensions", true),
+		// chromedp.Flag("disable-extensions", true),
+		// load-extension=/path/to/extension from https://stackoverflow.com/questions/66970005/c-sharp-selenium-enable-default-extensions
+		chromedp.Flag("load-extensions", filepath.Join(hd, "/Library/Application Support/Google/Chrome/Default/Extensions")),
 		chromedp.Flag("disable-features", "site-per-process,Translate,BlinkGenPropertyTrees"),
 		chromedp.Flag("disable-hang-monitor", true),
 		chromedp.Flag("disable-ipc-flooding-protection", true),
@@ -95,6 +103,8 @@ func Submit(fc *complaint.FullComplaint) error {
 		chromedp.Flag("enable-automation", true),
 		chromedp.Flag("password-store", "basic"),
 		chromedp.Flag("use-mock-keychain", true),
+
+		chromedp.UserDataDir(filepath.Join(hd, ".cache/cwc_chrome_profile")),
 	}
 
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
@@ -103,21 +113,96 @@ func Submit(fc *complaint.FullComplaint) error {
 	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
-	var url string
+	var url, title string
 	switch fc.VehicleType {
 	case reg.FHV.String():
 		url = "https://portal.311.nyc.gov/article/?kanumber=KA-01244"
+		title = "Car Service Complaint"
 	default:
 		url = "https://portal.311.nyc.gov/article/?kanumber=KA-01241"
+		title = "Taxi Complaint"
 	}
 
 	// navigate
 	fmt.Printf("> opening %s\n", url)
-	if err := chromedp.Run(ctx, chromedp.Navigate(url)); err != nil {
+	var loginLink string
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+	); err != nil {
 		return err
 	}
 
-	fmt.Printf("10s")
-	time.Sleep(10 * time.Second)
+	log.Printf("waiting for page %q", title)
+	if err := chromedp.Run(ctx,
+		// chromedp.WaitVisible(fmt.Sprintf(`//h1[text()=%q]]`, title)),
+		chromedp.TextContent(`.login-area > a`, &loginLink, chromedp.ByQuery),
+	); err != nil {
+		return err
+	}
+	log.Printf("login link %#v", loginLink)
+
+	if loginLink == "Sign In" {
+		fmt.Printf("> Sign In\n")
+		if err := chromedp.Run(ctx,
+			chromedp.Click(`.login-area > a`, chromedp.NodeVisible, chromedp.ByQuery),
+		); err != nil {
+			return err
+		}
+
+		var currentAddress string
+		for currentAddress != url {
+			time.Sleep(500 * time.Millisecond)
+			if err := chromedp.Run(ctx,
+				chromedp.Location(&currentAddress),
+			); err != nil {
+				return err
+			}
+		}
+		log.Printf("checking title")
+		if err := chromedp.Run(ctx); // chromedp.WaitVisible(fmt.Sprintf(`//h1[text()=%q]]`, title)),
+		err != nil {
+			return err
+		}
+
+		fmt.Printf("> authenticated")
+	}
+	switch fc.VehicleType {
+	case reg.FHV.String():
+		err = SubmitFHV(ctx, fc)
+	default:
+		err = SubmitTaxi(ctx, fc)
+	}
+	if err != nil {
+		return err
+	}
+	input.Ask("Done?", "")
+	return nil
+}
+
+func SubmitTaxi(ctx context.Context, fc *complaint.FullComplaint) error {
+	// click = "You were not a passenger"
+	return fmt.Errorf("not implemented")
+}
+
+func SubmitFHV(ctx context.Context, fc *complaint.FullComplaint) error {
+	log.Printf("SubmitFHV")
+	start := `//h5/a[text()[contains(.,"you were NOT a passenger")]]`
+	if err := chromedp.Run(ctx,
+		chromedp.Click(`//button/div/div[text()='Driver Complaint']`),
+	); err != nil {
+		return err
+	}
+	log.Printf("click start %s", start)
+	if err := chromedp.Run(ctx,
+		chromedp.WaitVisible(start),
+		chromedp.Click(start),
+	); err != nil {
+		return err
+	}
+
+	// chromedp.Nodes(`document`, &nodes, chromedp.ByJSPath),
+	// chromedp.Click(`#example-After`, chromedp.NodeVisible),
+	// chromedp.Value(`#example-After textarea`, &example),
+	// chromedp.TextContent
 	return nil
 }
